b9cbd0413f3ad0c99572aeebd319d804dba90bd2
Leteno <changchunjunhao@gmail.com>
2025-06-29 16:10:34 +0800

    Build path for popup.js to call function in content_script

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
 console.log("Content script loaded");
+
+function stupidGame() {
+  console.log("Stupic game started");
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "startGame") {
+    console.log("Starting stupid game");
+    stupidGame();
+    sendResponse({ status: "Game started" });
+  } else {
+    sendResponse({ status: "Unknown action" });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
 document.getElementById('clickMe').addEventListener('click', () => {
-  alert('Button clicked!');
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    console.log("Hello world!");
+    chrome.tabs.sendMessage(tabs[0].id, { action: "startGame" }, (response) => {
+      console.log("Response from content script:", response);
+    });
+  });
 });
